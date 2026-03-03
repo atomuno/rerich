c3b71f66aa03b851a79351f00188f77db465b062
user <user@machine.local>
2026-03-03 17:44:11 +0300

    Switch deploy from GitHub Pages to VPS

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  // ЗАМЕНИТЕ 'charity-portal' на точное название вашего репозитория на GitHub
-  basePath: '/rerich', 
-  assetPrefix: '/rerich',
-  images: { unoptimized: true }, 
+
+
+  images: { 
+    unoptimized: true 
+  }, 
   reactCompiler: true,
 };
 
